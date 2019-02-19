88f1eb427d26d7dd32a955b91d8f421e12f42331
YagoDel <yagovalleinclan@gmail.com>
2019-02-19 11:21:51 +0900

    First commit. Basic structure and some initial utils.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name='microcavities',
+      version='1.0',
+      description='Microcavities experiments and simulations',
+      url='',
+      license='',
+      packages=find_packages(),
+      zip_safe=False)
