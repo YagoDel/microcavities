dce2e4cb0ccc5e8530456d8fed6cd238998f32f2
YagoDel <yagovalleinclan@gmail.com>
2019-08-19 14:40:55 +0900

    Script for taking and analysing a low-power dispersion for measuring the energy, mass and lifetime of the lower polariton

---
+++ microcavities/experiment/scripts/dispersion.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+from microcavities.analysis.analysis_functions import dispersion, find_k0
+from microcavities.experiment.utils import spectrometer_calibration, magnification
+
+wvl = spectrometer.wavelength
+dispersion_img = pvcam.raw_image()
+bkg = pvcam.background
+try:
+    dispersion_img -= bkg
+except Exception as e:
+    print 'Failed to background image. Should not matter'
+
+wvls = spectrometer_calibration(wavelength=wvl)
+energy_axis = 1240 / wvls
+
+mag = magnification([0.01, 0.25, 0.1, 0.1, 0.2])[0]
+k0 = int(find_k0(dispersion_img))
+k_axis = np.linspace(-200, 200, 400)
+k_axis -= -200+k0
+k_axis *= 20 * 1e-6 / mag  # Converting to SI and dividing by magnification
+k_axis *= 1e-6  # converting to inverse micron
+
+results, args, kwargs = dispersion(dispersion_img, k_axis, energy_axis, True)
