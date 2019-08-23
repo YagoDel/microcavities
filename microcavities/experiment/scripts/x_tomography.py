050a4b76b0dcd029d1c71d3cd1714c8412250f78
YagoDel <yagovalleinclan@gmail.com>
2019-08-23 12:53:01 +0900

    Fixes #7 Tomography now accepts unit-full ranges

--- microcavities/experiment/scripts/x_tomography.py
+++ microcavities/experiment/scripts/x_tomography.py
@@ -4,15 +4,15 @@ from nplab.utils.gui import QtWidgets, QtCore, uic, get_qt_app
 from microcavities.utils.HierarchicalScan import ExperimentScan
 from microcavities.experiment.utils import magnification
 
-center_steps = 6898000  # This value might need to be changed if the setup is realigned
+center_steps = 6617980  # This value might need to be changed if the setup is realigned
 
-stages.toggle('k_lens', 'on')
+stages.toggle('k_lens', 'off')
 stages.move(center_steps, 'spectrometer_lens')
 
-mag = magnification([0.01, 0.25, 0.1, 0.1, 0.2])[0]
-k_fac = 20 * 1e-6 / mag  # Converting to SI and dividing by magnification
-k_fac *= 1e-6  # converting to inverse micron
-stage_steps_to_inverse_micron = 327*k_fac/660000.  # the diameter of k-space on the camera is 327 pixels
+mag = magnification([0.01, 0.25, 0.1, 0.2])[0]
+x_fac = 20 * 1e-6 / mag  # Converting to SI and dividing by magnification
+x_fac /= 1e-6  # converting to micron
+stage_steps_to_inverse_micron = 150*x_fac/300000.  # 150 pixels is 300000 steps
 
 
 class BasicInput(QtWidgets.QMainWindow):
@@ -28,8 +28,8 @@ class BasicInput(QtWidgets.QMainWindow):
         central_widget.setLayout(layout)
         # labels = ['Minimum', 'Maximum', '# measurements', '# images']
         # defaults = ['6568000', '7228000', '100', '1']
-        labels = ['k_range (inv. micron)', '# measurements', '# images']
-        defaults = ['6.585', '100', '1']
+        labels = ['x_range (micron)', '# measurements', '# images']
+        defaults = ['100', '100', '1']
         self.lineedits = [QtWidgets.QLineEdit(name) for name in defaults]
         self.labels = [QtWidgets.QLabel(name) for name in labels]
 
@@ -43,9 +43,9 @@ app = get_qt_app()
 bi = BasicInput()
 bi.show()
 
-krange = float(bi.lineedits[0].text())
-min_steps = int(center_steps - krange/(2*stage_steps_to_inverse_micron))
-max_steps = int(center_steps + krange/(2*stage_steps_to_inverse_micron))
+x_range = float(bi.lineedits[0].text())
+min_steps = int(center_steps - x_range/(2*stage_steps_to_inverse_micron))
+max_steps = int(center_steps + x_range/(2*stage_steps_to_inverse_micron))
 n_steps = int(bi.lineedits[1].text())
 n_images = int(bi.lineedits[-1].text())
 
