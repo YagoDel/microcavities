385e9cb9209575664e53452f8fd1a39cbe1774d2
YagoDel <yagovalleinclan@gmail.com>
2019-08-29 18:46:00 +0900

    Cleanup and better readme

--- setup.py
+++ setup.py
@@ -1,9 +1,23 @@
 from setuptools import setup, find_packages
 
+with open('README.md', 'r') as readme:
+    long_description = readme.read()
+
 setup(name='microcavities',
+      author='Yago Del',
       version='1.0',
       description='Microcavities experiments and simulations',
-      url='',
-      license='',
+      long_description=long_description,
+      url='https://github.com/YagoDel/microcavities',
+      license='MIT',
       packages=find_packages(),
+      install_requires=[
+          'numpy<2.0dev',
+          'matplotlib<3.0dev',
+          'h5py<3.0',
+          'pyyaml<4.0',
+          'pymsgbox<2.0',
+          'nidaqmx<1.0',
+          'tmm<1.0'
+      ],
       zip_safe=False)
