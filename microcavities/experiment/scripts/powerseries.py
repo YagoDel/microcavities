f62cee3614debc200b7b7dc46eed0077d6f996ba
YagoDel <yagovalleinclan@gmail.com>
2020-10-07 09:57:56 +0900

    Powerseries scanning GUI. Tested for dispersion datasets

--- microcavities/experiment/scripts/powerseries.py
+++ microcavities/experiment/scripts/powerseries.py
@@ -1,14 +1,15 @@
 # -*- coding: utf-8 -*-
 from nplab.utils.gui import QtWidgets, QtCore, uic, get_qt_app
 from microcavities.utils.HierarchicalScan import ExperimentScan
-from microcavities.utils import get_data_path
-import time
+from microcavities.utils import get_data_path, get_data_directory
+from microcavities.analysis.characterisation import dispersion_power_series
 import os
-import sys
 import yaml
+import pymsgbox
 
 
 data_path = get_data_path()
+data_directory = get_data_directory()
 app = get_qt_app()
 
 
@@ -19,35 +20,46 @@ def qt_wait(max_wait=120.0, interval=0.1):
         print('hey')
 
 
-class BasicInput(QtWidgets.QWidget):
-    finished_setup = QtCore.Signal()
+class PowerSeries(QtWidgets.QWidget):
+    """GUI for multi-section power series data taking and analysis
+    Should be run inside the kernel of a microcavities.experiment.run.Experiment
+
+    Can set the instrument and variable names for changing the power and the camera name and function name for taking
+    images.
+    Each section in the power series has a different range of powers, and a different exposure value for the camera.
+    Background images still need to be taken afterwards.
+    """
 
     def __init__(self):
-        super(BasicInput, self).__init__()
+        super(PowerSeries, self).__init__()
         self.n_sections = 0
         self.lineedits = []
         self.labels = None
-        uic.loadUi((os.path.dirname(__file__) + '/k_powerseries.ui'), self)
-        self.lineEdit_filename.setText(data_path)
+        uic.loadUi(os.path.join(os.path.dirname(__file__), 'powerseries.ui'), self)
 
+        # Set default data path
+        self.lineEdit_filename.setText(data_path.lstrip(data_directory))
+
+        # Add column labels
         labels = ['Minimum', 'Maximum', '# measurements', '# images', 'exposure']
         self.labels = [QtWidgets.QLabel(name) for name in labels]
         [self.gridLayout_sections.addWidget(lbl, 0, idx) for lbl, idx in zip(self.labels, range(len(self.labels)))]
 
-        self.spinBox_sections.valueChanged.connect(self.update_sections)
+        self._connect_gui()
+
+    def _connect_gui(self):
+        self.spinBox_sections.valueChanged.connect(self._update_sections_gui)
         self.pushButton_yamls.pressed.connect(self.make_yamls)
         self.pushButton_run.pressed.connect(self.run)
+        self.pushButton_analyse.pressed.connect(self.analyse)
 
-        # self.setup_ui()
-        # self.finished_setup.connect(self.run, type=QtCore.Qt.QueuedConnection)
-
-    def update_sections(self):
+    def _update_sections_gui(self):
+        """Add/remove rows for each of the power series sections"""
         try:
             exposure = exper[self.lineEdit_camera.text()].exposure
         except:
             exposure = 0.5
         defaults = ['0.01', '0.07', '7', '3', '%.2g' % exposure]
-        lineedit = [QtWidgets.QLineEdit(name) for name in defaults]
         n_sections = int(self.spinBox_sections.value())
         if n_sections > self.n_sections:
             for row in range(self.n_sections, n_sections):
@@ -63,21 +75,7 @@ class BasicInput(QtWidgets.QWidget):
 
     @property
     def filename(self):
-        return str(self.lineEdit_filename.text())
-    #
-    # def setup_ui(self):
-    #     centralWidget = QtWidgets.QWidget()
-    #     layout = QtWidgets.QGridLayout()
-    #     centralWidget.setLayout(layout)
-    #     labels = ['Instrument', 'Minimum', 'Maximum', '# measurements', '# images', 'exposure']
-    #     defaults = ['power_wheel', '0.01', '0.15', '8', '1']
-    #     self.lineedits = [QtWidgets.QLineEdit(name) for name in defaults]
-    #     self.labels = [QtWidgets.QLabel(name) for name in labels]
-    #
-    #     for idx, wdgt, lbl in zip(list(range(5)), self.lineedits, self.labels):
-    #         layout.addWidget(wdgt, idx, 1)
-    #         layout.addWidget(lbl, idx, 0)
-    #     self.setCentralWidget(centralWidget)
+        return os.path.normpath(self.lineEdit_filename.text())
 
     def make_yamls(self):
         for index in range(self.n_sections):
@@ -98,63 +96,43 @@ class BasicInput(QtWidgets.QWidget):
                                                   float(lineedit[1].text()), int(lineedit[2].text())])],
                           measurements=measurements
                           )
-        directory = os.path.dirname(self.filename)
-        with open(os.path.join(directory, 'yamls', 'dispersion_%d.yaml' % index), 'w') as file:
+        filename = os.path.basename(self.filename).rstrip('.h5')
+        if os.path.isabs(self.filename):
+            directory = os.path.dirname(self.filename)
+        else:
+            directory = os.path.dirname(os.path.join(data_directory, self.filename))
+        if not os.path.exists(os.path.join(directory, 'yamls')):
+            os.mkdir(os.path.join(directory, 'yamls'))
+        with open(os.path.join(directory, 'yamls', '%s_series_%d.yaml' % (filename, index)), 'w') as file:
             yaml.dump(dictionary, file)
         return
 
     def run(self):
         self.make_yamls()
         directory = os.path.dirname(self.filename)
+        filename = os.path.basename(self.filename).rstrip('.h5')
         for index in range(self.n_sections):
             exposure = float(self.lineedits[index][-1].text())
             exper[self.lineEdit_camera.text()].exposure = exposure
             try:
-                yaml_path = os.path.join(directory, 'yamls', 'dispersion_%d.yaml' % index)
+                yaml_path = os.path.join(directory, 'yamls', '%s_series_%d.yaml' % (filename, index))
                 scan = ExperimentScan(yaml_path, exper, gui)
                 scan.run()
             except Exception as e:
                 print('Failed running at %d because: %s' % (index, e))
-        return
+
+            toggle = pymsgbox.confirm('Prepare next run: nd filters', 'Confirm abort', ["Continue", 'Stop'])
+            if toggle == "Stop":
+                break
+
+    def analyse(self):
+        directory = os.path.dirname(self.filename)
+        filename = os.path.basename(self.filename).rstrip('.h5')
+        yaml_paths = [os.path.join(directory, 'yamls', '%s_series_%d.yaml' % (filename, index)) for index in range(self.n_sections)]
+        if self.lineEdit_analysistype.text() == 'dispersion':
+            fig, axs = dispersion_power_series(yaml_paths, None, 0, exper['spectrometer'].wavelength, '1200')
+            fig.show()
 
 
-bi = BasicInput()
-bi.show()
-# sys.exit(app.exec_())
-
-# while not bi.isHidden():
-#     time.sleep(0.1)
-#     app.processEvents()
-#
-# values = [float(x.text()) for x in bi.lineedits[1:-1]]
-# instr = bi.lineedits[0].text()
-# n_images = int(bi.lineedits[-1].text())
-#
-# params = dict(save_type="local",
-#               variables=[dict(instrument=instr, property='power', values=['linear'] + values)],
-#               measurements=[
-#                   dict(instrument='pvcam', function='raw_image', name='img%d' % (x + 1),
-#                        kwargs=dict(update_latest_frame=True), save=True) for x in range(n_images)
-#               ],
-#               analysis_functions=[
-#                   dict(data_name='img%d' % (x + 1), function_name='raw_img%d' % (x + 1)) for x in range(n_images)
-#               ]
-#               )
-# scan = ExperimentScan(params, exper, gui)
-# scan.run_modally()
-# data = np.array([scan.results['img%d' % (x+1)] for x in range(n_images)])
-#
-# # The following presumes you have run the dispersion script, which defines k0 and energy_axis
-# mean_data = np.mean(data, 0)
-# k0_data = mean_data[:, k0]
-#
-# normalised = []
-# for row in k0_data:
-#     row = np.copy(row)
-#     row -= np.percentile(row, 0.1)
-#     row /= np.percentile(row, 99.9)
-#     normalised += [row]
-# normalised = np.array(normalised)
-#
-# plt.imshow(normalised.transpose(), aspect='auto', extent=[values[0], values[1], energy_axis[-1], energy_axis[0]])
-#
+ps = PowerSeries()
+ps.show()
