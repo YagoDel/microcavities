445ac3bf80b511b547251224e063e1b9e89310b4
Yago <yagovalleinclan@gmail.com>
2021-03-30 10:46:53 +0900

    Ordering file

--- microcavities/utils/plotting.py
+++ microcavities/utils/plotting.py
@@ -13,6 +13,7 @@ from mpl_toolkits.axes_grid1.axes_divider import make_axes_locatable
 plt.style.use(os.path.join(os.path.dirname(__file__), 'default_style.mplstyle'))
 
 
+# Utils
 def default_save(figure, name, base_path=None):
     if base_path is None:
         base_path = os.path.dirname(get_data_path(None, False))
@@ -22,169 +23,12 @@ def default_save(figure, name, base_path=None):
     figure.savefig(os.path.join(base_path, 'figures', '%s.png' % name), dpi=1200, bbox_inches='tight')
 
 
-def imshow(img, ax=None, diverging=True, scaling=None, xaxis=None, yaxis=None, cbar=True, cbar_kwargs=None,
-           xlabel=None, ylabel=None, **kwargs):
-    """Utility imshow, wraps commonly used plotting tools
-
-    :param img: 2D array
-    :param ax: pyplot.axes
-    :param diverging: whether to use a diverging colormap, centered around 0
-    :param scaling: a 2-tuple or a float. The pixel to unit conversion value
-    :param cbar: whether to add a colorbar
-    :param cbar_label: str
-    :param xlabel: str
-    :param ylabel: str
-    :param kwargs: any other named arguments are passed to plt.imshow
-    :return:
-    """
+def _make_axes(ax=None):
     if ax is None:
         fig, ax = plt.subplots(1, 1)
     else:
         fig = ax.figure
-
-    if xaxis is None:
-        xaxis = np.arange(-0.5, img.shape[1]+0.5, dtype=np.float)
-    if yaxis is None:
-        yaxis = np.arange(-0.5, img.shape[0]+0.5, dtype=np.float)
-    if scaling is not None:
-        try:
-            xaxis *= scaling[0]
-            yaxis *= scaling[1]
-        except:
-            xaxis *= scaling
-            yaxis *= scaling
-    kwargs['extent'] = [xaxis[0], xaxis[-1], yaxis[0], yaxis[-1]]
-
-    if diverging:
-        kwargs['cmap'] = 'RdBu'
-        val = np.max(np.abs([np.max(img), np.min(img)]))
-        if 'vmin' in kwargs and 'vmax' not in kwargs:
-                kwargs['vmax'] = -kwargs['vmin']
-        elif 'vmin' not in kwargs and 'vmax' in kwargs:
-                kwargs['vmin'] = -kwargs['vmax']
-        else:
-            kwargs['vmin'] = -val
-            kwargs['vmax'] = val
-
-    im = ax.imshow(img, **kwargs)
-    if cbar:
-        if cbar_kwargs is None:
-            cbar_kwargs = dict()
-
-        ax_divider = make_axes_locatable(ax)
-        if 'orientation' in cbar_kwargs:
-            if cbar_kwargs['orientation'] == 'horizontal':
-                cax = ax_divider.append_axes("top", size="7%", pad="2%")
-            else:
-                cax = ax_divider.append_axes("right", size="7%", pad="2%")
-        else:
-            cax = ax_divider.append_axes("right", size="7%", pad="2%")
-
-        fig.colorbar(im, cax=cax, **cbar_kwargs)
-        cax.xaxis.tick_top()
-
-    if xlabel is not None: ax.set_xlabel(xlabel)
-    if ylabel is not None: ax.set_ylabel(ylabel)
-
     return fig, ax
-
-
-def colorful_imshow(images, ax=None, norm_args=(0, 100), from_black=True, cmap='hsv', *args, **kwargs):
-    normed = np.array([normalize(x, norm_args) for x in images])
-
-    _cmap = cm.get_cmap(cmap, normed.shape[0] + 1)
-    full = np.zeros(images.shape[1:] + (4,))
-    for idx in range(normed.shape[0]):
-        tst = np.tile(normed[idx], (4, 1, 1))
-        plain_color = np.tile(_cmap(idx), images.shape[1:] + (1, ))
-        full += plain_color * np.moveaxis(tst, 0, -1)
-
-    if from_black:
-        r = full[..., 0]
-        g = full[..., 1]
-        b = full[..., 2]
-        full = np.moveaxis(np.array([r, g, b]), 0, -1)
-
-    kwargs['diverging'] = False
-    kwargs['cbar'] = False
-    return imshow(full, ax, *args, **kwargs), _cmap(range(normed.shape[0]))
-
-
-def imshow_transparency(img, alpha=None, percentiles=(0, 100), vmin=None, vmax=None,
-                        diverging=True, cbar=False, cmap='coolwarm_r', ax=None,
-                        *args, **kwargs):
-    if ax is None:
-        fig, ax = plt.subplots(1, 1)
-    else:
-        fig = ax.figure
-    if diverging:
-        if vmin is not None or vmax is not None:
-            warnings.warn('Both diverging and vmin/vmax given. Defaulting to diverging.')
-        val = np.max(np.abs([img.min(), img.max()]))
-        norm_colour = colors.Normalize(-val, +val)
-    else:
-        norm_colour = colors.Normalize(vmin, vmax)
-
-    img_array = plt.get_cmap(cmap)(norm_colour(img))
-
-    if alpha is not None:
-        norm_alpha = colors.Normalize(np.percentile(alpha, percentiles[0]), np.percentile(alpha, percentiles[1]))
-        img_array[..., 3] = norm_alpha(alpha)
-
-    kwargs['diverging'] = False
-    kwargs['cbar'] = False
-    kwargs['ax'] = ax
-    kwargs['vmin'] = None
-    kwargs['vmax'] = None
-    imshow(img_array, *args, **kwargs)
-
-    cbar_ax = None
-    if cbar:
-        _cbar = fig.colorbar(cm.ScalarMappable(norm=norm_colour, cmap=cmap), ax=ax)
-        cbar_ax = _cbar.ax
-    return fig, ax, cbar_ax
-
-
-def combined_imshow(images, axes=(0, ), normalise=False, normalise_kwargs=None, *args, **kwargs):
-    """    For making arrays of images, faster than making tons of subplots.
-    Makes a large array with NaNs to separate different images, which can then be plotted in a single Matplotlib artist
-
-    :param images:
-    :param axes:
-    :param normalise:
-    :param normalise_kwargs:
-    :param args:
-    :param kwargs:
-    :return:
-    """
-    shape = images.shape
-    assert len(shape) - len(axes) == 2
-    other_axis = set(range(len(shape))) - set(axes)
-    if len(axes) == 1:
-        a, b = square(shape[axes[0]])
-    elif len(axes) == 2:
-        a, b = [shape[idx] for idx in axes]
-    else:
-        raise NotImplementedError("Images shape %s and axes %s don't combine" % (shape, axes))
-
-    stepx, stepy = [images.shape[x] for x in other_axis]
-    combined_image = np.zeros((a * (stepx + 1), b * (stepy + 1))) + np.nan
-    for idx in range(a):
-        for idx2 in range(b):
-            if len(axes) == 1:
-                indx = idx * a + idx2
-                img = images[indx]
-            elif len(axes) == 2:
-                img = images[idx, idx2]
-            else:
-                raise ValueError
-            img = np.array(img, dtype=np.float)
-            if normalise:
-                if normalise_kwargs is None:
-                    normalise_kwargs = dict()
-                img = normalize(img, **normalise_kwargs)
-            combined_image[idx * stepx + idx:(idx + 1) * stepx + idx, idx2 * stepy + idx2:(idx2 + 1) * stepy + idx2] = img
-    return imshow(combined_image, *args, **kwargs)
 
 
 def subplots(datas, plotting_func, axes=(0, ), fig_shape=None, figsize=8, sharex=False, sharey=False,
@@ -261,17 +105,44 @@ def subplots(datas, plotting_func, axes=(0, ), fig_shape=None, figsize=8, sharex
     return fig, axs, gs
 
 
+def label_grid(figure, grid, label, position, offset=0.07, **kwargs):
+    """Simple labelling of matplotlib.gridspec grids
+
+    :param figure: matplotlib.figure
+    :param grid: matplotlib.gridspec
+    :param label: string
+    :param position: string
+    :param offset: float
+    :return:
+    """
+    _pos = grid.get_grid_positions(figure)
+    if position == 'bottom':
+        figure.text(np.mean(_pos[2:]), _pos[0][-1]-offset, label, va='top', ha='center', **kwargs)
+    elif position == 'top':
+        figure.text(np.mean(_pos[2:]), _pos[1][0]+offset, label, va='bottom', ha='center', **kwargs)
+    elif position == 'left':
+        figure.text(_pos[2][0]-offset, np.mean(_pos[:2]), label, va='center', ha='right', rotation=90, **kwargs)
+    elif position == 'right':
+        figure.text(_pos[3][-1]+offset, np.mean(_pos[:2]), label, va='center', ha='left', rotation=-90, **kwargs)
+
+
+def unique_legend(ax, *args, **kwargs):
+    """Removes repeated labels in a legend"""
+    handles, labels = plt.gca().get_legend_handles_labels()
+    by_label = OrderedDict(zip(labels, handles))
+    ax.legend(by_label.values(), by_label.keys(), *args, **kwargs)
+
+
+# 1D plots
 def waterfall(lines, ax=None, cmap=None, xaxis=None, offset=None,
               labels=None, label_kwargs=None,
               xlabel=None, ylabel=None,
               peak_positions=None, peak_kwargs=None,
               **kwargs):
+    fig, ax = _make_axes(ax)
     if offset is None:
         offset = 1.05 * np.abs(np.min(np.diff(lines, axis=0)))
-    if ax is None:
-        fig, ax = plt.subplots(1, 1)
-    else:
-        fig = ax.figure
+
     default_label_kwargs = dict(ha='right', va='bottom')
     if label_kwargs is None:
         label_kwargs = dict()
@@ -320,44 +191,6 @@ def waterfall(lines, ax=None, cmap=None, xaxis=None, offset=None,
     return fig, ax
 
 
-def pcolormesh(img, x, y, ax=None, cbar=True, cbar_label=None, diverging=True, xlabel=None, ylabel=None, *args, **kwargs):
-    if ax is None:
-        fig, ax = plt.subplots(1, 1)
-    else:
-        fig = ax.figure
-    if diverging:
-        val = np.max(np.abs([np.max(img), np.min(img)]))
-        vmin = -val
-        vmax = val
-        if 'vmin' not in kwargs:
-            kwargs['vmin'] = vmin
-        if 'vmax' not in kwargs:
-            kwargs['vmax'] = vmax
-        if 'cmap' not in kwargs:
-            kwargs['cmap'] = 'RdBu'
-    sort_indxs_x = np.argsort(x)
-    sort_indxs_y = np.argsort(y)
-    _sorted_img = img[sort_indxs_x]
-    sorted_img = _sorted_img[:, sort_indxs_y]
-    x = x[sort_indxs_x]
-    y = y[sort_indxs_y]
-
-    middle_edges_x = np.mean([x[:-1], x[1:]], 0)
-    middle_edges_y = np.mean([y[:-1], y[1:]], 0)
-
-    edges_x = [x[0] - (middle_edges_x[0] - x[0])] + list(middle_edges_x) + [x[-1] + (x[-1] - middle_edges_x[-1])]
-    edges_y = [y[0] - (middle_edges_y[0] - y[0])] + list(middle_edges_y) + [y[-1] + (y[-1] - middle_edges_y[-1])]
-
-    im = ax.pcolormesh(edges_x, edges_y, sorted_img.transpose(), *args, **kwargs)
-
-    if cbar:
-        cb = fig.colorbar(im, ax=ax, label=cbar_label)
-        ax = (ax, cb.ax)
-    if xlabel is not None: ax.set_xlabel(xlabel)
-    if ylabel is not None: ax.set_ylabel(ylabel)
-    return fig, ax
-
-
 def colorline(y, z=None, xaxis=None, ax=None, cmap='copper', vmin=None, vmax=None, *args, **kwargs):
     """
     http://nbviewer.ipython.org/github/dpsanders/matplotlib-examples/blob/master/colorline.ipynb
@@ -366,10 +199,8 @@ def colorline(y, z=None, xaxis=None, ax=None, cmap='copper', vmin=None, vmax=Non
     Optionally specify colors in the array z
     Optionally specify a colormap, a norm function and a line width
     """
-    if ax is None:
-        fig, ax = plt.subplots(1, 1)
-    else:
-        fig = ax.figure
+    fig, ax = _make_axes(ax)
+
     if xaxis is None:
         xaxis = np.arange(len(y))
 
@@ -410,32 +241,204 @@ def _make_segments(x, y):
     return segments
 
 
-def label_grid(figure, grid, label, position, offset=0.07, **kwargs):
-    """Simple labelling of matplotlib.gridspec grids
+# 2D plots
+def imshow(img, ax=None, diverging=True, scaling=None, xaxis=None, yaxis=None, cbar=True, cbar_kwargs=None,
+           xlabel=None, ylabel=None, **kwargs):
+    """Utility imshow, wraps commonly used plotting tools
 
-    :param figure: matplotlib.figure
-    :param grid: matplotlib.gridspec
-    :param label: string
-    :param position: string
-    :param offset: float
+    :param img: 2D array
+    :param ax: pyplot.axes
+    :param diverging: whether to use a diverging colormap, centered around 0
+    :param scaling: a 2-tuple or a float. The pixel to unit conversion value
+    :param cbar: whether to add a colorbar
+    :param cbar_label: str
+    :param xlabel: str
+    :param ylabel: str
+    :param kwargs: any other named arguments are passed to plt.imshow
     :return:
     """
-    _pos = grid.get_grid_positions(figure)
-    if position == 'bottom':
-        figure.text(np.mean(_pos[2:]), _pos[0][-1]-offset, label, va='top', ha='center', **kwargs)
-    elif position == 'top':
-        figure.text(np.mean(_pos[2:]), _pos[1][0]+offset, label, va='bottom', ha='center', **kwargs)
-    elif position == 'left':
-        figure.text(_pos[2][0]-offset, np.mean(_pos[:2]), label, va='center', ha='right', rotation=90, **kwargs)
-    elif position == 'right':
-        figure.text(_pos[3][-1]+offset, np.mean(_pos[:2]), label, va='center', ha='left', rotation=-90, **kwargs)
+    fig, ax = _make_axes(ax)
+
+    if xaxis is None:
+        xaxis = np.arange(-0.5, img.shape[1]+0.5, dtype=np.float)
+    if yaxis is None:
+        yaxis = np.arange(-0.5, img.shape[0]+0.5, dtype=np.float)
+    if scaling is not None:
+        try:
+            xaxis *= scaling[0]
+            yaxis *= scaling[1]
+        except:
+            xaxis *= scaling
+            yaxis *= scaling
+    kwargs['extent'] = [xaxis[0], xaxis[-1], yaxis[0], yaxis[-1]]
+
+    if diverging:
+        kwargs['cmap'] = 'RdBu'
+        val = np.max(np.abs([np.max(img), np.min(img)]))
+        if 'vmin' in kwargs and 'vmax' not in kwargs:
+                kwargs['vmax'] = -kwargs['vmin']
+        elif 'vmin' not in kwargs and 'vmax' in kwargs:
+                kwargs['vmin'] = -kwargs['vmax']
+        else:
+            kwargs['vmin'] = -val
+            kwargs['vmax'] = val
+
+    im = ax.imshow(img, **kwargs)
+    if cbar:
+        if cbar_kwargs is None:
+            cbar_kwargs = dict()
+
+        ax_divider = make_axes_locatable(ax)
+        if 'orientation' in cbar_kwargs:
+            if cbar_kwargs['orientation'] == 'horizontal':
+                cax = ax_divider.append_axes("top", size="7%", pad="2%")
+            else:
+                cax = ax_divider.append_axes("right", size="7%", pad="2%")
+        else:
+            cax = ax_divider.append_axes("right", size="7%", pad="2%")
+
+        fig.colorbar(im, cax=cax, **cbar_kwargs)
+        cax.xaxis.tick_top()
+
+    if xlabel is not None: ax.set_xlabel(xlabel)
+    if ylabel is not None: ax.set_ylabel(ylabel)
+
+    return fig, ax
 
 
-def unique_legend(ax, *args, **kwargs):
-    handles, labels = plt.gca().get_legend_handles_labels()
-    by_label = OrderedDict(zip(labels, handles))
-    ax.legend(by_label.values(), by_label.keys(), *args, **kwargs)
+def colorful_imshow(images, ax=None, norm_args=(0, 100), from_black=True, cmap='hsv', *args, **kwargs):
+    normed = np.array([normalize(x, norm_args) for x in images])
 
+    _cmap = cm.get_cmap(cmap, normed.shape[0] + 1)
+    full = np.zeros(images.shape[1:] + (4,))
+    for idx in range(normed.shape[0]):
+        tst = np.tile(normed[idx], (4, 1, 1))
+        plain_color = np.tile(_cmap(idx), images.shape[1:] + (1, ))
+        full += plain_color * np.moveaxis(tst, 0, -1)
+
+    if from_black:
+        r = full[..., 0]
+        g = full[..., 1]
+        b = full[..., 2]
+        full = np.moveaxis(np.array([r, g, b]), 0, -1)
+
+    kwargs['diverging'] = False
+    kwargs['cbar'] = False
+    return imshow(full, ax, *args, **kwargs), _cmap(range(normed.shape[0]))
+
+
+def imshow_transparency(img, alpha=None, percentiles=(0, 100), vmin=None, vmax=None,
+                        diverging=True, cbar=False, cmap='coolwarm_r', ax=None,
+                        *args, **kwargs):
+    # fig, ax = _make_axes(ax)
+
+    if diverging:
+        if vmin is not None or vmax is not None:
+            warnings.warn('Both diverging and vmin/vmax given. Defaulting to diverging.')
+        val = np.max(np.abs([img.min(), img.max()]))
+        norm_colour = colors.Normalize(-val, +val)
+    else:
+        norm_colour = colors.Normalize(vmin, vmax)
+
+    img_array = plt.get_cmap(cmap)(norm_colour(img))
+
+    if alpha is not None:
+        norm_alpha = colors.Normalize(np.percentile(alpha, percentiles[0]), np.percentile(alpha, percentiles[1]))
+        img_array[..., 3] = norm_alpha(alpha)
+
+    kwargs['diverging'] = False
+    kwargs['cbar'] = False
+    # kwargs['ax'] = ax
+    kwargs['vmin'] = None
+    kwargs['vmax'] = None
+    fig, ax = imshow(img_array, *args, **kwargs)
+
+    cbar_ax = None
+    if cbar:
+        _cbar = fig.colorbar(cm.ScalarMappable(norm=norm_colour, cmap=cmap), ax=ax)
+        cbar_ax = _cbar.ax
+    return fig, ax, cbar_ax
+
+
+def combined_imshow(images, axes=(0, ), normalise=False, normalise_kwargs=None, *args, **kwargs):
+    """    For making arrays of images, faster than making tons of subplots.
+    Makes a large array with NaNs to separate different images, which can then be plotted in a single Matplotlib artist
+
+    :param images:
+    :param axes:
+    :param normalise:
+    :param normalise_kwargs:
+    :param args:
+    :param kwargs:
+    :return:
+    """
+    shape = images.shape
+    assert len(shape) - len(axes) == 2
+    other_axis = set(range(len(shape))) - set(axes)
+    if len(axes) == 1:
+        a, b = square(shape[axes[0]])
+    elif len(axes) == 2:
+        a, b = [shape[idx] for idx in axes]
+    else:
+        raise NotImplementedError("Images shape %s and axes %s don't combine" % (shape, axes))
+
+    stepx, stepy = [images.shape[x] for x in other_axis]
+    combined_image = np.zeros((a * (stepx + 1), b * (stepy + 1))) + np.nan
+    for idx in range(a):
+        for idx2 in range(b):
+            if len(axes) == 1:
+                indx = idx * a + idx2
+                img = images[indx]
+            elif len(axes) == 2:
+                img = images[idx, idx2]
+            else:
+                raise ValueError
+            img = np.array(img, dtype=np.float)
+            if normalise:
+                if normalise_kwargs is None:
+                    normalise_kwargs = dict()
+                img = normalize(img, **normalise_kwargs)
+            combined_image[idx * stepx + idx:(idx + 1) * stepx + idx, idx2 * stepy + idx2:(idx2 + 1) * stepy + idx2] = img
+    return imshow(combined_image, *args, **kwargs)
+
+
+def pcolormesh(img, x, y, ax=None, cbar=True, cbar_label=None, diverging=True, xlabel=None, ylabel=None, *args, **kwargs):
+    fig, ax = _make_axes(ax)
+
+    if diverging:
+        val = np.max(np.abs([np.max(img), np.min(img)]))
+        vmin = -val
+        vmax = val
+        if 'vmin' not in kwargs:
+            kwargs['vmin'] = vmin
+        if 'vmax' not in kwargs:
+            kwargs['vmax'] = vmax
+        if 'cmap' not in kwargs:
+            kwargs['cmap'] = 'RdBu'
+    sort_indxs_x = np.argsort(x)
+    sort_indxs_y = np.argsort(y)
+    _sorted_img = img[sort_indxs_x]
+    sorted_img = _sorted_img[:, sort_indxs_y]
+    x = x[sort_indxs_x]
+    y = y[sort_indxs_y]
+
+    middle_edges_x = np.mean([x[:-1], x[1:]], 0)
+    middle_edges_y = np.mean([y[:-1], y[1:]], 0)
+
+    edges_x = [x[0] - (middle_edges_x[0] - x[0])] + list(middle_edges_x) + [x[-1] + (x[-1] - middle_edges_x[-1])]
+    edges_y = [y[0] - (middle_edges_y[0] - y[0])] + list(middle_edges_y) + [y[-1] + (y[-1] - middle_edges_y[-1])]
+
+    im = ax.pcolormesh(edges_x, edges_y, sorted_img.transpose(), *args, **kwargs)
+
+    if cbar:
+        cb = fig.colorbar(im, ax=ax, label=cbar_label)
+        ax = (ax, cb.ax)
+    if xlabel is not None: ax.set_xlabel(xlabel)
+    if ylabel is not None: ax.set_ylabel(ylabel)
+    return fig, ax
+
+
+# Tests
 
 def test_waterfall():
     fig, axs = plt.subplots(1, 5, figsize=(8, 4))
