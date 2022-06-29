db2859e22f95ac2453d16347c2da13c1df61d50a
Yago <yagovalleinclan@gmail.com>
2022-06-29 20:09:28 +0900

    Moved SortigKey to HierarchicalDatastructure

--- microcavities/analysis/utils.py
+++ microcavities/analysis/utils.py
@@ -1,34 +1,10 @@
 # -*- coding: utf-8 -*-
 
 from microcavities.utils.plotting import *
-import ast
 from scipy.ndimage import gaussian_filter
 from scipy.stats import zscore
 from microcavities.utils.functools import lorentzianNd, gaussianNd
 import collections
-
-
-def SortingKey(pattern):
-    """
-    To be used to sort the names of folders, assuming the folder names follow the convention 'ParamName=ParamValue'
-    or 'ParamName=ParamValue-ParamIteration' if there is more than one dataset with the same parameter value.
-    Sorting is done in order of ParamValue, and then ParamIteration
-    Args:
-        pattern: name of the folder
-    Returns:
-        Either a single value or a two-tuple that is used for the sort
-    """
-    splitpat = pattern.split('=')
-    returnval = (bool(len(splitpat) - 1),)
-    if len(splitpat) > 1:
-        if re.match('.\d*-\d+|.*0\.\d*-\d+', splitpat[1]):
-            returnval += tuple([np.real(ast.literal_eval(x)) for x in re.findall('(.+)-(\d+)', splitpat[1])[0]])
-        elif re.match('\(\d+\.*\d*, *\d+.*\d*\)', splitpat[1]):
-            returnval += tuple([np.real(ast.literal_eval(x)) for x in re.findall('(\d+\.*\d*),(\d+\.*\d*)', splitpat[1])[0]])
-        else:
-            val = ast.literal_eval(splitpat[1])
-            returnval += (np.sign(np.real(val)) * np.abs(val),)
-    return returnval
 
 
 def fit_function(name):
