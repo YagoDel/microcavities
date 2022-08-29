968ddbfd1b3286fedcdeb4f7e5f1830fb107c435
Yago <yagovalleinclan@gmail.com>
2022-08-29 15:35:31 +0900

    Updated rotation analysis

--- microcavities/analysis/burningbus/rotation.py
+++ microcavities/analysis/burningbus/rotation.py
@@ -1,9 +1,86 @@
 # -*- coding: utf-8 -*-
-import numpy as np
-
 from microcavities.utils.plotting import *
+from microcavities.utils.plotting import create_axes
+from nplab.utils.log import create_logger
+import h5py
+from scipy.ndimage.measurements import center_of_mass
+from microcavities.analysis.phase_maps import low_pass
+from microcavities.utils import apply_along_axes
+from functools import partial
+from microcavities.experiment.utils import magnification_function, spectrometer_calibration
+
+LOGGER = create_logger('Rotation')
+
+cdict = {'red': [(0.0, 0.0, 1.0),
+                 (0.25, 0.0, 0.0),
+                 (0.5, 1.0, 1.0),
+                 (0.75, 1.0, 1.0),
+                 (1.0, 0.0, 0.0)],
+         'green': [(0.0, 0.0, 1.0),
+                   (0.25, 0.4, 0.4),
+                   (0.5, 1.0, 1.0),
+                   (0.75, 0.0, 0.0),
+                   (1.0, 0.0, 0.0)],
+         'blue': [(0.0, 1.0, 1.0),
+                  (0.25, 0.0, 0.0),
+                  (0.5, 0.0, 0.0),
+                  (0.75, 0.0, 0.0),
+                  (1.0, 0.0, 0.0)]}
+
+viscm = colors.LinearSegmentedColormap('viscm', cdict, 256)
+
+n_cmap = viscm #'viscm'
+delta_n_cmap = 'RdBu_r'
+lz_cmap = 'PuOr'
+
+pixels_per_step = 2 * 150 / 300000
+
+data_path = get_data_path('2022_01_26/raw_data.h5')
+figure_data_path = get_data_path('2022_01_26/figure_data.h5')
+rotation_path = '/Users/yago/Desktop/DDrive/Papers/Rotation/'
+mu = '\u03BC'
+Delta = '\u0394'
+hbar_u = '\u0127'
+momentum_scale = 0.019634954084936207
+spatial_scale = 0.4
+hbar = 6.582119569 * 10 ** (-16) * 10 ** 3 * 10 ** 12   # in meV.ps
+c = 3 * 10 ** 14 * 10 ** -12                            # Speed of Light   um/ps
 
 
+"""# Defining spatial axis"""
+x_axis = np.linspace(2020000.0, 2060000.0, 11)
+y_axis = np.linspace(6155000.0, 6195000.0, 11)
+x_axis -= np.mean(x_axis)
+x_axis *= pixels_per_step
+x_axis *= spatial_scale
+y_axis -= np.mean(y_axis)
+y_axis *= pixels_per_step
+y_axis *= spatial_scale
+y_axis = y_axis[::-1]
+frequencies = [-7.5, -5, -3, -2, -1, -0.5, -0.1, 0.1, 0.5, 1, 2, 3, 5, 7.5]
+# x_axis -= 0.8
+# y_axis += 2.5
+
+"""Definining k axis"""
+#  Finding k=0
+with h5py.File(data_path, 'r') as dfile:
+    below_threshold = np.array([(dfile['full_scan1/below_threshold/%s' % name][...]-110) for name in ['matisse', 'toptica']])
+lp = np.array([low_pass(x, 0.05) for x in below_threshold])
+k0_CoM = np.array([center_of_mass(x) for x in lp])
+k0_argmax = np.array([np.argmax(np.mean(lp, ax), axis=1) for ax in [-1, -2]])
+k0_eye = [155, 169]
+with h5py.File(data_path, 'r') as dfile:
+    bkg = dfile['full_scan1/just_toptica_bkg'][...]
+    static_condensate = np.asarray(dfile['full_scan1/just_toptica'][...], float) - bkg
+    static_condensate = static_condensate[..., ::-1, ::-1]  # inverting the k-plane (from the k-lens)
+
+ky = ((np.arange(static_condensate.shape[-2]) - k0_eye[0]) * momentum_scale)[::-1]
+kx = ((np.arange(static_condensate.shape[-1]) - k0_eye[1]) * momentum_scale)
+kx_func = partial(np.interp, xp=np.arange(static_condensate.shape[-1]), fp=kx)
+ky_func = partial(np.interp, xp=np.arange(static_condensate.shape[-2]), fp=ky)
+
+
+"""Utility functions"""
 def jacobian(vector_field, axes=None):
     """Numerical Jacobian of an ND vector field
 
@@ -32,6 +109,33 @@ def curl(vector_field, axes=None):
     return jcb[1, 0] - jcb[0, 1]  # TODO: this only works for 2D
 
 
+def test_curl_divergence():
+    x = np.linspace(-25, 25, 101) + 1e-6
+    y = np.linspace(-50, 50, 201) + 1e-6
+    extent = [y[0], y[-1], x[-1], x[0]]
+    X, Y = np.meshgrid(x, y, indexing='ij')
+    R = np.sqrt(X**2 + Y**2)
+    T = np.arctan2(Y, X)
+
+    lines = (X / X, 0*X)
+    vortex = (X / R, Y / R)
+    vortex2 = (-X / R, -Y / R)
+    source = (-Y / R, X / R)
+    source2 = (Y / R, -X / R)
+
+    fields = [lines, source, source2, vortex, vortex2]
+
+    fig, axs = plt.subplots(3, len(fields), sharex=True, sharey=True)
+    for idx, field in enumerate(fields):
+        div = divergence(field, (x, y))
+        crl = curl(field, (x, y))
+        axs[0, idx].quiver(Y[::10, ::10], X[::10, ::10], field[0][::10, ::10], field[1][::10, ::10])
+        imshow(div, axs[1, idx], xaxis=y, yaxis=x)
+        imshow(crl, axs[2, idx], xaxis=y, yaxis=x)
+    for ax in axs.flatten():
+        ax.set_aspect('equal')
+
+
 def vortex_field(x, y, scale=1):
     Y, X = np.meshgrid(x, y)
     theta = np.arctan2(Y, X)
@@ -52,11 +156,43 @@ def saddle_field(x, y, scale=1):
     return scale*r * np.cos(theta), scale * r * np.sin(theta)
 
 
-def angular_momentum_classical(vector_field, axes=None):
+def photon_density(camera_count):
+    optical_losses = 0.97  # at the dichroic
+    optical_losses *= (0.97**2)  # estimated reflection from 2 mirrors
+    # optical_losses *= (0.95**4)  # estimated transmission through 4 lenses
+    # optical_losses *= 0.1  # estimated spectrometer grating
+    optical_losses *= 0.026  # directly measured
+    nd_filter = 1.5e-6  # we used ND6
+    quantum_efficiency = 0.4  # 75  # camera QE
+    efficiency = nd_filter * quantum_efficiency * optical_losses / 2  # factor of two from both directions in the mcav
+
+    exposure_time = 2  # in seconds
+    alpha = 2  # CCD
+    photon_flux = camera_count * alpha / (efficiency * exposure_time)
+
+    lifetime = 14e-12  #2.1e-12  # 2.5e-12
+    hopfield = 0.09  # 0.025
+    polariton = photon_flux * lifetime / hopfield
+
+    # Spatial filtering with a 50um pinhole
+    # radius = 50 / magnification_function([0.01, 0.25], wavelength=800e-9)[0]
+    # area = (np.pi * radius**2)
+
+    return polariton   # , polariton / area
+
+
+def angular_momentum_array(vector_field, axes=None):
     if axes is None:
-        axes = np.array([(np.arange(x)-x/2) for x in vector_field[0].shape[::-1]])
-    Axes = np.array(np.meshgrid(*axes))
-    return np.cross(Axes, vector_field, axis=0)
+        axes = np.array([(np.arange(x)-x/2) for x in vector_field[0].shape])
+        X, Y = np.meshgrid(*axes)
+    else:
+        X, Y = axes
+    return vector_field[0] * Y - vector_field[1] * X  # hbar *
+
+
+def angular_momentum_average(vector_field, density, axes=None):
+    ll = angular_momentum_array(vector_field, axes)
+    return hbar * np.average(ll, (0, 1), density)
 
 
 def angular_momentum_quantum(wavefunction, axes=None):
@@ -87,3 +223,52 @@ def momentum_quantum(wavefunction, axes=None):
         axes = np.array([(np.arange(x) - x / 2) for x in wavefunction.shape[::-1]])
     vector_field = np.array(np.gradient(wavefunction, *axes))
     return np.imag(np.conj(wavefunction) * vector_field)[::-1]
+
+
+def get_data(frequency, norm=False):
+    with h5py.File(data_path, 'r') as dfile:
+        bkg = np.array(dfile['full_scan1/bkg'])
+        data = np.array(dfile['full_scan1/f=%gGHz' % frequency][...]) - bkg
+    data = data[..., ::-1, ::-1]
+    if norm:
+        data = normalize(data, axis=(-2, -1))
+    return data
+
+
+def measure_momenta(image, plott=True, ax=None):
+    image = normalize(image)
+    mask = image > 0.5
+    CoM = np.squeeze(np.array(center_of_mass(image, mask, 1)))
+    if plott:
+        fig, ax = create_axes(ax)
+        imshow(image, ax, cbar=False, xaxis=kx, yaxis=ky)
+        ax.plot([0, kx_func(CoM[1])], [0, ky_func(CoM[0])], 'k')
+    return kx_func(CoM[1]), ky_func(CoM[0])
+
+
+def get_field(data):
+    spatial_image = np.mean(data, (-2, -1))
+    momenta = np.array(apply_along_axes(partial(measure_momenta, plott=False), (2, 3), data, 2))
+    return spatial_image, momenta
+
+
+def plot_quiver(data, ax=None, **kwargs):
+    fig, ax = create_axes(ax)
+    spatial_image, momenta = get_field(data)
+    imshow(spatial_image, ax, cbar=False, xaxis=x_axis, yaxis=y_axis, aspect='equal')
+    ax.quiver(x_axis, y_axis, momenta[0], momenta[1], **kwargs)
+    ax.quiver(x_axis, y_axis, momenta[0], momenta[1], **kwargs)
+    return fig, ax, momenta
+
+
+def get_differential_field(frequency):
+    img_p, k_p = get_field(get_data(frequency))
+    img_m, k_m = get_field(get_data(-frequency))
+
+    return np.mean([img_p, img_m], 0), k_p - k_m
+
+
+def plot_differential(frequency, **kwargs):
+    img, field = get_differential_field(frequency)
+    _, ax = imshow(img, xaxis=x_axis, yaxis=y_axis, cbar=False)
+    ax.quiver(x_axis, y_axis, field[0], field[1], **kwargs)
