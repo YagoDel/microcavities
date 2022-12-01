20967443e56c6882ea8a164a1aeefc8c270bc177
Yago <yagovalleinclan@gmail.com>
2022-12-01 15:30:08 +0900

    Cleaned solver code and added test function

--- microcavities/simulations/coupling_model_2D.py
+++ microcavities/simulations/coupling_model_2D.py
@@ -14,10 +14,6 @@ from dxf.lattices import *
 
 hbar = 0.658  # in meV ps
 electron_mass = 5.68e3  # in meV ps2 um-2
-mass = 1e-5
-# size = 250
-# region = 35
-# x_spacing = 2*region/(size-1)
 
 
 def make_axes(region, n_points):
@@ -35,49 +31,59 @@ def make_axes(region, n_points):
     return x, y, _x, _y
 
 
-def solve(size,mass,electron_mass,x_spacing,V):
+def kinetic_matrix(mass, size, x_spacing=1):
     diag = np.ones([size])
     diags = np.array([diag, -2*diag, diag])
-    #D is a sparse matrix which has -2 on the main diagonal and 1 on the two neighbouring diagonals
-    D = sparse.spdiags(diags, np.array([-1,0,1]), size, size)
-    T = -1/2 * sparse.kronsum(D,D)
-    T_dim = T*hbar**2/ (mass*electron_mass*x_spacing**2)
-    #V is a 2D grid reshaped into N**2 vector to be stretched along main diagonal
-    U = sparse.diags(V.reshape(size**2), (0))
-    H = T_dim+U   #N**2 x N**2 dimension
-    #compute eigenvalues and eigenvectors
-    vals, vecs = eigsh(H, k=250, which='SA')  #SM means we are looking for only the smallest eigenvalues
-    return vals,vecs
+    # D is a sparse matrix which has -2 on the main diagonal and 1 on the two neighbouring diagonals
+    D = sparse.spdiags(diags, np.array([-1, 0, 1]), size, size)
+    T = -1/2 * sparse.kronsum(D, D)
+    return T * (hbar**2) / (mass*electron_mass*x_spacing**2)
+
+
+def solve(V, mass, axes, n_eigenvalues=250):
+    size = V.shape[0]
+    x_spacing = np.diff(axes[3])[0]
+    if n_eigenvalues > size**2: n_eigenvalues = (size**2)-1
+
+    # Create a matrix for Kinetic energy
+    T = kinetic_matrix(mass, size, x_spacing)
+    # V is a 2D grid reshaped into N**2 vector to be stretched along main diagonal
+    U = sparse.diags(V.reshape(size**2), 0)
+    # Create the full Hamiltonian
+    H = T + U   # N**2 x N**2 dimension
+
+    # Compute eigenvalues and eigenvectors
+    vals, vecs = eigsh(H, k=n_eigenvalues, which='SA')  # SM means we are looking for only the smallest eigenvalues
+    return vals, vecs
 
 
 def get_eigenvector(n, vecs, size=None):
     """
-
     :param n: int. Index selection of eigenvector
     :param vecs: array. List of eigenvectors
     :param size: int. To determine how to reshape the eigenvector from a list to a matrix
     :return:
     """
-    if size is None: size = int(np.sqrt(len(vecs[0])))
-    return vecs.T[n].reshape((size,size))
+    if size is None: size = int(np.sqrt(vecs.shape[0]))
+    return vecs.T[n].reshape((size, size))
 
 
 def plot_eigenvectors(x, y, vecs):
    for n in range(0, 4):
        fig = plt.figure(figsize=(6,6))
        plt.contourf(x, y, get_eigenvector(n, vecs), 20)
-       plt.colorbar(label = "psi")#plot PDF with 20 countour levels by e()**2 with 20 countor levels
+       plt.colorbar(label="psi")#plot PDF with 20 countour levels by e()**2 with 20 countor levels
        return fig
 
 
 """POTENTIAL FUNCTIONS"""
 
 
-def harmonic_potential(omega, axes=None):
+def harmonic_potential(omega, mass, axes=None):
     if axes is None:
         axes = make_axes(30, 101)
-
-    return 0.5 * (omega**2) * (axes[0]**2 + axes[1]**2)
+    potential = 0.5 * mass * (omega**2) * (axes[0]**2 + axes[1]**2)
+    return potential, axes
     # nx,ny = np.arange(0,4), np.arange(0,4)
     # ana_vals = hbar**2*(nx+ny+1)*omega**2/(2*mass)
     # return V
@@ -151,6 +157,37 @@ def test_potential_circles():
 
     V, axes = double_circle(3, -10, 4)
     imshow(V, axs[2], xaxis=axes[2], yaxis=axes[3], aspect='equal')
+
+
+def test_solver():
+    """Using the analytical formula for a QHO, makes sure the numerical solver is giving correct results"""
+    axes = make_axes(30, 51)
+
+    omega = 1e-3
+    mass = 1e-2
+    potential, axes = harmonic_potential(omega, mass * electron_mass, axes)
+
+    analytical_eigenvalues = np.array([[hbar*omega*(nx+ny+1)
+                                        for nx in np.arange(0, 4)]
+                                       for ny in np.arange(0, 4)]).flatten()
+    analytical_eigenvalues = np.sort(analytical_eigenvalues)
+
+    numerical_eigenvalues, eigenvectors = solve(potential, mass, axes)
+
+    # fig, axs = plt.subplots(2, 3)
+    fig = figure(2)
+    _gs = gridspec.GridSpec(2, 1, fig)
+    gs0 = gridspec.GridSpecFromSubplotSpec(1, 2, _gs[0], width_ratios=[1, 2])
+    axs0 = gs0.subplots()
+    gs1 = gridspec.GridSpecFromSubplotSpec(1, 3, _gs[1])
+    axs1 = gs1.subplots()
+    imshow(potential, axs0[0], xaxis=axes[2], yaxis=axes[3])
+    axs0[1].plot(analytical_eigenvalues, 'k--', label='analytical')
+    axs0[1].plot(numerical_eigenvalues[:len(analytical_eigenvalues)], 'r.-', label='numerical')
+    axs0[1].legend()
+
+    for idx in range(3):
+        imshow(get_eigenvector(idx, eigenvectors), axs1[idx], xaxis=axes[2], yaxis=axes[3])
 
 
 if __name__ == "__main__":
